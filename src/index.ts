4ad9d8c5253704d0a192a8049da5c6ecb4da553c
Dongjie Wu <dongjie.wu@protonmail.com>
2025-06-18 09:41:46 +1000

    fix export server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
 // Create an MCP server
-const server = new McpServer({
+export const server = new McpServer({
   name: "demo-server",
   version: "1.0.0"
 });
@@ -35,7 +34,3 @@ server.resource(
     }]
   })
 );
-
-// Start receiving messages on stdin and sending messages on stdout
-const transport = new StdioServerTransport();
-await server.connect(transport);
